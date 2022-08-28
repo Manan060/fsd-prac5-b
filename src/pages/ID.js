ae0f7fd8bc62971ca535a9947a6cbeb753840f75
Manan <97335966+Manan060@users.noreply.github.com>
2022-08-28 13:01:19 +0530

    Add files via upload

---
+++ src/pages/ID.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const ID = () => {
+  return (
+    <div
+      style={{
+        display: 'relative',
+        flexDirection:'column',
+        alignSelf:'right'
+      }}
+    >
+      <b>20CS060_Manan Pathak</b>
+    </div>
+  );
+};
+
+export default ID;
